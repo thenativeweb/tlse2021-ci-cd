4843ca34dc8725e41176de9ed145f99d0a75a119
Alexander "dotKuro" Kampf <mail@akampf.dev>
2021-08-31 17:22:36 +0200

    fix: fix qa for backend

--- backend/lib/index.ts
+++ backend/lib/index.ts
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
+import cors from 'cors';
 import express from 'express';
 import { flaschenpost } from 'flaschenpost';
 import http from 'http';
 import path from 'path';
-import { processenv } from "processenv";
-import cors from 'cors';
+import { processenv } from 'processenv';
 
 const logger = flaschenpost.getLogger();
 const app = express();
+
 app.use(cors());
 app.get('/logo', (req, res): void => {
   res.sendFile(path.join(__dirname, '..', 'logo.png'));
 });
 app.get('/logo_description', (req, res): void => {
-  const response = processenv('LOGO_DESCRIPTION', 'thenativeweb')
+  const response = processenv('LOGO_DESCRIPTION', 'thenativeweb');
+
   res.send(response);
 });
 const server = http.createServer(app);
